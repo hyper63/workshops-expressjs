b8c047c15ff8e7f96972db0191856700262c7d53
Tom Wilson <twilson63@gmail.com>
2021-10-25 15:21:33 +0000

    added setup to connect workshop

---
+++ connect/setup.js
@@ -0,0 +1,28 @@
+import connect from 'hyper-connect'
+
+const hyper = connect(process.env['HYPER'])()
+
+await hyper.data.bulk([
+  {
+    "id": "character-1",
+    "type": "character",
+    "name": "Mario"
+  },
+  {
+    "id": "character-2",
+    "type": "character",
+    "name": "Luigi"
+  },
+  {
+    "id": "character-3",
+    "type": "character",
+    "name": "Princess Peach"
+  },
+  {
+    "id": "character-4",
+    "type": "character",
+    "name": "Bowser"
+  }
+])
+
+console.log(await hyper.data.list())
